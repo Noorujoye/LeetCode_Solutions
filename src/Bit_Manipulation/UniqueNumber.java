b50312d00d869f3ef86bfb056f0f144a468a314e
Noorain Warsi <joyenooru@gmail.com>
2025-09-01 12:36:30 +0530

    solved leet code 136 Unique Number

---
+++ src/Bit_Manipulation/UniqueNumber.java
@@ -0,0 +1,18 @@
+package Bit_Manipulation;
+
+public class UniqueNumber {
+
+    public static int singleNumber(int[] nums) {
+
+        int uNum = nums[0];
+        for (int i=1; i<nums.length; i++) {
+            uNum = uNum ^ nums[i];
+        }
+        return uNum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1,2,3,4};
+        System.out.println(singleNumber(nums));
+     }
+}
