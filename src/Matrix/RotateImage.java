26c3fa832bb800db4f4eac42fee1c6fff69256ed
Noorain Warsi <joyenooru@gmail.com>
2025-09-04 12:26:34 +0530

    solve rotate image

---
+++ src/Matrix/RotateImage.java
@@ -0,0 +1,49 @@
+package Matrix;
+
+public class RotateImage {
+    public static void rotate(int[][] matrix) {
+
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int start = 0, end = n - 1;
+
+            while (start < end) {
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+        };
+
+        rotate(matrix);
+
+        // Print the rotated matrix
+        for (int[] row : matrix) {
+            for (int x : row) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
