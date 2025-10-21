c79c8c90dd106cc7b04a9d488b994b9491124845
Noorain <joyenooru@gmail.com>
2025-10-22 00:36:38 +0530

    power of two

---
+++ src/Bit_Manipulation/PowerOfTwo.java
@@ -0,0 +1,23 @@
+package Bit_Manipulation;
+
+public class PowerOfTwo {
+    public static Boolean powerOfTwo(int n) {
+        //    Base condition
+//        if (n == 0) return false;
+//        while (n % 2 == 0) {
+//            n/=2;
+//        }
+//        return n == 1;
+
+        //2nd approach best
+        if (n==0) return false;
+        long x = (long)n;
+        return x == (x & -x); //& bitwise operator
+    }
+    public static void main(String[] args) {
+        int n = 16;
+
+        System.out.println(PowerOfTwo.powerOfTwo(n));
+
+    }
+}
