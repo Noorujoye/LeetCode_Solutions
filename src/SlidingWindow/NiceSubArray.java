f55e5332bdd8063070905020db61af5e3030d896
Noorain <joyenooru@gmail.com>
2025-11-04 23:27:07 +0530

    find a number of sub array which has k odd numbers

---
+++ src/SlidingWindow/NiceSubArray.java
@@ -0,0 +1,29 @@
+package SlidingWindow;
+
+public class NiceSubArray {
+    public int numberOfSubarrays(int[] nums, int k) {
+        return atMost(nums, k) - atMost(nums, k - 1);
+    }
+    public int atMost(int[] nums ,int k) {
+
+        int start = 0, oddCount = 0, count = 0;
+        for (int end=0; end<nums.length; end++) {
+            int value = nums[end];
+            if (value % 2 == 1) {
+                oddCount++;
+            }
+            if (oddCount > k) {
+                if (nums[start] % 2 == 1) oddCount--;
+                start++;
+            }
+            count += end - start + 1;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,1,2,1,1};
+        NiceSubArray n = new NiceSubArray();
+        System.out.println(n.numberOfSubarrays(arr , 3));
+
+    }
+}
