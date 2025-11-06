b6fc76d55891275eb054531a2a00da0ca43a7389
Noorain <joyenooru@gmail.com>
2025-11-06 12:51:51 +0530

    optimized it

--- src/SlidingWindow/LongestRepeatingCharacterReplacement.java
+++ src/SlidingWindow/LongestRepeatingCharacterReplacement.java
@@ -2,6 +2,42 @@ package SlidingWindow;
 
 public class LongestRepeatingCharacterReplacement {
     public int characterReplacement(String s, int k) {
+        int maxLen=0;
+        int changes;
+        int start=0;
+        int maxF = 0;
+        int[] hash = new int[26];
+
+        for (int end=0; end<s.length(); end++) {
+
+            hash[s.charAt(end) - 'A']++;
+
+            maxF = Math.max(maxF , hash[s.charAt(end)-'A']);
+
+            changes = (end-start+1) - maxF;
+
+            if (changes > k) {
+                hash[s.charAt(start) - 'A']--;
+                start++;
+            }
+
+            maxLen = Math.max(maxLen , end-start+1);
+        }
+        return maxLen;
+    }
+    public static void main(String[] args) {
+        String s = "AAAABBBB";
+        int k = 1;
+        LongestRepeatingCharacterReplacement l = new LongestRepeatingCharacterReplacement();
+        System.out.println(l.characterReplacement(s , k));
+    }
+}
+
+
+// Brute force
+
+/*
+    public int characterReplacement(String s, int k) {
         int maxLen = 0;
         int changes;
         int n = s.length();
@@ -24,10 +60,4 @@ public class LongestRepeatingCharacterReplacement {
         }
         return maxLen;
     }
-    public static void main(String[] args) {
-        String s = "AAAABBBB";
-        int k = 1;
-        LongestRepeatingCharacterReplacement l = new LongestRepeatingCharacterReplacement();
-        System.out.println(l.characterReplacement(s , k));
-    }
-}
+ */
