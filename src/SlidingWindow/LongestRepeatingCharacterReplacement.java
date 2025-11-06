47c5566cab838f7915fc37af640f9d8e29980de8
Noorain <joyenooru@gmail.com>
2025-11-06 12:32:52 +0530

    so i learned how i can return the length of substring which is made my replacing the less occurr character by most occurring charcter but only kth changes u can make

---
+++ src/SlidingWindow/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,33 @@
+package SlidingWindow;
+
+public class LongestRepeatingCharacterReplacement {
+    public int characterReplacement(String s, int k) {
+        int maxLen = 0;
+        int changes;
+        int n = s.length();
+        for (int i=0; i<n; i++) {
+            int[] hash = new int[26];
+            int maxF = 0;
+            for (int j=i; j<n; j++) {
+                hash[s.charAt(j) - 'A']++;
+                maxF = Math.max(maxF , hash[s.charAt(j)-'A']);
+
+                //in current window how many different character needs to be changed
+                changes = (j-i+1) - maxF;
+
+                if (changes <= k) {
+                    maxLen = Math.max(maxLen , j-i+1);
+                } else {
+                    break;
+                }
+            }
+        }
+        return maxLen;
+    }
+    public static void main(String[] args) {
+        String s = "AAAABBBB";
+        int k = 1;
+        LongestRepeatingCharacterReplacement l = new LongestRepeatingCharacterReplacement();
+        System.out.println(l.characterReplacement(s , k));
+    }
+}
