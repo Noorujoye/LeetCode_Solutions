abc732ea363bf361e77717d8dc822c63ef8a43e3
Noorain <joyenooru@gmail.com>
2025-12-29 12:37:42 +0530

    reverse a linedlist

--- src/LinkedList/ReverseALinkedList.java
+++ src/LinkedList/ReverseALinkedList.java
@@ -3,7 +3,7 @@ import java.util.LinkedList;
 
 
 public class ReverseALinkedList {
-    public static class Node {
+    public static    class Node {
         private int data;
         private Node next;
 
