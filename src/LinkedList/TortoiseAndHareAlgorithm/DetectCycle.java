6f726c4716ff1377032751220b2549c09c4dc643
Noorain <joyenooru@gmail.com>
2026-01-06 11:04:38 +0530

    detect a cycle , where the cycle begins return that node if not return null

---
+++ src/LinkedList/TortoiseAndHareAlgorithm/DetectCycle.java
@@ -0,0 +1,75 @@
+package LinkedList.TortoiseAndHareAlgorithm;
+
+public class DetectCycle {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+    Node tail;
+    int size;
+
+    // Add at end
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    // Utility: print list (VERY IMPORTANT for debugging)
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+
+    public static Node detectCycle(Node head) {
+        if (head == null || head.next == null) return null;
+
+
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        DetectCycle d = new DetectCycle();
+        d.addLast(1);
+        d.addLast(2);
+        d.addLast(3);
+        d.addLast(4);
+        d.addLast(5);
+        d.printList();
+        d.head = DetectCycle.detectCycle(d.head);
+        d.printList();
+    }
+}
