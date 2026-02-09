d60f531ab8326767c24c9cbe87fd3fad6307fbce
Noorain <joyenooru@gmail.com>
2026-02-09 18:51:34 +0530

    patterns

---
+++ src/Patterns.java
@@ -0,0 +1,235 @@
+import java.util.Stack;
+
+public class Patterns {
+    public static void main(String[] args) {
+        /*
+        ****
+        ****
+        ****
+        ****
+         */
+        for (int i=0; i<4; i++) {
+            for (int j=0; j<4; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("\n" + "\n" + "\n");
+
+
+
+        /*
+         *
+         **
+         ***
+         ****
+         */
+        for (int i=0; i<4; i++) {
+            for (int j=0; j<i+1; j++) { // for j<=i can also be the condition
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("\n" + "\n" + "\n");
+
+
+        /*
+         1
+         12
+         123
+         1234
+         */
+        for (int i=0; i<10; i++) {
+            for (int j=0; j<=i; j++) {
+                System.out.print(j+1 + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("\n" + "\n" + "\n");
+
+
+        /*
+         1
+         22
+         333
+         4444
+         */
+        for (int i=1; i<=4; i++) {
+            for (int j=1; j<=i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("\n" + "\n" + "\n");
+
+
+        /*
+         ****
+         ***
+         **
+         *
+         */
+        for (int i=4; i>0; i--) {
+          for (int j=1; j<=i; j++) {
+              System.out.print("* ");
+          }
+            System.out.println();
+        }
+        System.out.println("\n" + "\n" + "\n");
+
+
+        /*
+         12345
+         1234
+         123
+         12
+         1
+         */
+        for (int i=5; i>0; i--) {
+           for (int j=1; j<=i; j++) {
+               System.out.print(j);
+           }
+            System.out.println();
+        }
+        System.out.println("\n" + "\n" + "\n");
+
+
+        /*
+         *
+        * *
+      *  *  *
+    *   *  *  *
+    what we are printing is space star space
+         */
+        int n = 5;
+        for (int i=0; i<n; i++) {
+            // space n-i-1
+            for (int j=0; j<n-i-1; j++) {
+                System.out.print(" ");
+            }
+            // star
+            for (int k = 0; k < 2 * i +1; k++) {
+                System.out.print("*");
+            }
+
+            //space
+            for (int y = 0 ; y < n-i -1; y++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.println("\n" + "\n" + "\n");
+
+
+
+          /*
+        *******
+         *****
+          ***
+           *
+    what we are printing is space star space
+         */
+        n = 5;
+        for (int i=n; i>0; i--) {
+            // space n-i-1
+            for (int j=1; j < n-i+1; j++) {
+                System.out.print(" ");
+            }
+            // star
+            for (int k = 0; k < 2 * i -1; k++) {
+                System.out.print("*");
+            }
+            //space
+            for (int y = 0 ; y < n-i+1; y++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.println("\n" + "\n" + "\n");
+
+
+        /*
+           *
+          ***
+        *******
+       *********
+       *********
+        ********
+        ******
+         ***
+          *
+         */
+        n = 5;
+        for (int i=0; i<n; i++) {
+            // space n-i-1
+            for (int j=0; j<n-i-1; j++) {
+                System.out.print(" ");
+            }
+            // star
+            for (int k = 0; k < 2 * i +1; k++) {
+                System.out.print("*");
+            }
+
+            //space
+            for (int y = 0 ; y < n-i -1; y++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        n = 5;
+        for (int i=n; i>0; i--) {
+            // space n-i-1
+            for (int j=1; j < n-i+1; j++) {
+                System.out.print(" ");
+            }
+            // star
+            for (int k = 0; k < 2 * i -1; k++) {
+                System.out.print("*");
+            }
+            //space
+            for (int y = 0 ; y < n-i+1; y++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.println("\n" + "\n" + "\n");
+
+
+        /*
+        *
+        **
+        ***
+        ****
+        ***
+        **
+        *
+         */
+        n = 4;
+        for (int a=0; a<n; a++) {
+            for (int i=0; i<=a; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int a=0; a<=n-1; a++) {
+            for (int i=0; i<n-a-1; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("\n\n\n");
+
+//        for (int l=1; l< 2*n-1; l++) {
+//            int stars = l;
+//            if (l > n) {
+//                stars= 2*n-l;
+//
+//
+//            }
+//
+//            for (int j=0; j<)
+//        }
+
+
+    }
+}
